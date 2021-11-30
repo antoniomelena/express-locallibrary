919e2efaf7bd7cc4630eec979559fa28c49d6459
Antonio Melena <melenaantonio@gmail.com>
2021-11-30 00:29:38 -0600

    added book delete view, still not finished

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -195,7 +195,30 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Book delete GET");
+  async.parallel(
+    {
+      book: function (callback) {
+        Book.findById(req.params.id).exec(callback);
+      },
+      book_instances: function (callback) {
+        BookInstance.find({ book: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.book == null) {
+        // No results
+        res.redirect("/catalog/books");
+      }
+      res.render("book_delete", {
+        title: "Delete Book",
+        book: results.book,
+        book_instances: results.book_instances,
+      });
+    }
+  );
 };
 
 // Handle book delete on POST.
